706d3eb923f08c161b50a6b0eb95fd46e6365d4a
quling <quling@dist.com.cn>
2023-08-31 10:55:31 +0800

    新增eslint和prettier校验

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,39 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    "plugin:prettier/recommended"
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
+  plugins: ["import"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": "error",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-this-alias": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    // 使用三等号而不是双等号
+    eqeqeq: "warn",
+    // async函数中必须具有await表达式
+    "require-await": "error",
+    // import语句在模块顶部
+    "import/first": "error",
+    // 避免在多个地方重复引入同意模块
+    "import/no-duplicates": "error",
+    // 在import语句和非import语句直接插入一空行
+    "import/newline-after-import": "error"
   },
+  settings: {
+    "import/resolver": {
+      typescript: {}
+    }
+  }
 };
---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  printWidth: 80,
+  tabWidth: 2,
+  arrowParens: "always",
+  singleQuote: false,
+  trailingComma: "none",
+  useTabs: false,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css"
+}
